0805ab42b7598426954845f9d402b99b24e119f0
helana123 <helanajenson.3@gmail.com>
2024-03-12 15:33:15 +0530

    Initial commit

---
+++ lib/signin_signup_pages/login_with_email/login_with_email_model.dart
@@ -0,0 +1,54 @@
+import '/backend/api_requests/api_calls.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_radio_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/form_field_controller.dart';
+import 'login_with_email_widget.dart' show LoginWithEmailWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class LoginWithEmailModel extends FlutterFlowModel<LoginWithEmailWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  // State field(s) for emailAddress widget.
+  FocusNode? emailAddressFocusNode;
+  TextEditingController? emailAddressController;
+  String? Function(BuildContext, String?)? emailAddressControllerValidator;
+  // State field(s) for password widget.
+  FocusNode? passwordFocusNode;
+  TextEditingController? passwordController;
+  late bool passwordVisibility;
+  String? Function(BuildContext, String?)? passwordControllerValidator;
+  // State field(s) for RadioButton widget.
+  FormFieldController<String>? radioButtonValueController;
+  // Stores action output result for [Backend Call - API (loginMail)] action in Button widget.
+  ApiCallResponse? apiResultgsb;
+
+  /// Initialization and disposal methods.
+
+  @override
+  void initState(BuildContext context) {
+    passwordVisibility = false;
+  }
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+    emailAddressFocusNode?.dispose();
+    emailAddressController?.dispose();
+
+    passwordFocusNode?.dispose();
+    passwordController?.dispose();
+  }
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+
+  String? get radioButtonValue => radioButtonValueController?.value;
+}
